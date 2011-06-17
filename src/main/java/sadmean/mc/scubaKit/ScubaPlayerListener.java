c6c908e90aba42e65245f68e9fb0e30f4c22b3eb
Tyler McNair <sadmean@sadmean.com>
2011-06-17 13:56:34 -0600

    converted tics to seconds, added support for large tic values

--- src/main/java/sadmean/mc/scubaKit/ScubaPlayerListener.java
+++ src/main/java/sadmean/mc/scubaKit/ScubaPlayerListener.java
@@ -1,5 +1,6 @@
 package sadmean.mc.scubaKit;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerListener;
@@ -8,8 +9,9 @@ import org.bukkit.event.player.PlayerMoveEvent;
 
 public class ScubaPlayerListener extends PlayerListener {
 	
-
+	String chatStart = ChatColor.DARK_AQUA + "[ScubaKit] " +ChatColor.GRAY;
 	Player player;
+	String airEnder = " seconds of air remaining";
 	
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		player = event.getPlayer();
@@ -26,17 +28,25 @@ public class ScubaPlayerListener extends PlayerListener {
 		ScubaKit.log_It("finest", " Checking to see if a message needs to be sent to " + player.getName());
 		int air = player.getRemainingAir();
 		switch (air) {
-		case 2000: player.sendMessage("You have 2000 tics of air left"); break;
-		case 1500: player.sendMessage("You have 1500 tics of air left"); break;
-		case 1000: player.sendMessage("You have 1000 tics of air left"); break;
-		case 750: player.sendMessage("You have 750 tics of air left"); break;
-		case 600: player.sendMessage("You have 600 tics of air left"); break;
-		case 500: player.sendMessage("You have 500 tics of air left"); break;
-		case 400: player.sendMessage("You have 400 tics of air left"); break;
-		case 280: player.sendMessage("You have 300 tics of air left"); break;
-		case 200: player.sendMessage("You have 200 tics of air left"); break;
-		case 100: player.sendMessage("You have 100 tics of air left"); break;
-		case 0: player.sendMessage("YOU ARE OUT OF AIR!"); break;
+		case 10000: player.sendMessage(chatStart + "500" + airEnder); break;
+		case 9000: player.sendMessage(chatStart + "450" + airEnder); break;
+		case 8000: player.sendMessage(chatStart + "400" + airEnder); break;
+		case 7000: player.sendMessage(chatStart + "350" + airEnder); break;
+		case 6000: player.sendMessage(chatStart + "300" + airEnder); break;
+		case 5000: player.sendMessage(chatStart + "250" + airEnder); break;
+		case 4000: player.sendMessage(chatStart + "200" + airEnder); break;
+		case 3000: player.sendMessage(chatStart + "150" + airEnder); break;
+		case 2000: player.sendMessage(chatStart + "100" + airEnder); break;
+		case 1500: player.sendMessage(chatStart + "75" + airEnder); break;
+		case 1000: player.sendMessage(chatStart + "50" + airEnder); break;
+		case 800: player.sendMessage(chatStart + "40" + airEnder); break;
+		case 600: player.sendMessage(chatStart + "30" + airEnder); break;
+		case 500: player.sendMessage(chatStart + "25" + airEnder); break;
+		case 400: player.sendMessage(chatStart + "20" + airEnder); break;
+		case 300: player.sendMessage(chatStart + "15" + airEnder); break;
+		case 200: player.sendMessage(chatStart + "10" + airEnder); break;
+		case 100: player.sendMessage(chatStart + "5" + airEnder); break;
+		case 0: player.sendMessage(chatStart + ChatColor.RED + "YOU ARE OUT OF AIR!"); break;
 		default:
 			break;
 		}
